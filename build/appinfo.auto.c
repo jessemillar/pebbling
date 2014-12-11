28ff1349e9e596e8ef75d04d5d5c9ca725ec1081
Jesse Millar <hellojessemillar@gmail.com>
2014-12-11 12:48:49 -0700

    Initial commit

---
+++ build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Previewer",
+  .company = "Jesse Millar",
+  .icon_resource_id = RESOURCE_ID_IMAGE_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_WATCH_FACE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x02, 0xA1, 0x8C, 0xE9, 0xFF, 0x79, 0x4F, 0x70, 0x9E, 0xC7, 0xFB, 0xAA, 0xCB, 0xF4, 0x78, 0xCA },
+  .virtual_size = 0xb6b6
+};
