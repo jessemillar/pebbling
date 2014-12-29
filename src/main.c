5f9b5e13e6d7940fca0745160b8060151459fc96
Jesse Millar <hellojessemillar@gmail.com>
2014-12-28 21:50:32 -0600

    Trying more ghetto stuff

--- src/main.c
+++ src/main.c
@@ -3,66 +3,63 @@
 static Window *window;
 
 static BitmapLayer *s_image_layer_first;
-static GBitmap *s_image_first;
 static BitmapLayer *s_image_layer_second;
-static GBitmap *s_image_second;
 static BitmapLayer *s_image_layer_third;
-static GBitmap *s_image_third;
 static BitmapLayer *s_image_layer_fourth;
-static GBitmap *s_image_fourth;
 
-void set_digit(BitmapLayer *image_layer, GBitmap *image, char character)
+static GBitmap *s_image_digit_0;
+static GBitmap *s_image_digit_1;
+static GBitmap *s_image_digit_2;
+static GBitmap *s_image_digit_3;
+static GBitmap *s_image_digit_4;
+static GBitmap *s_image_digit_5;
+static GBitmap *s_image_digit_6;
+static GBitmap *s_image_digit_7;
+static GBitmap *s_image_digit_8;
+static GBitmap *s_image_digit_9;
+
+void set_digit(BitmapLayer *image_layer, char character)
 {
 	if (character == '0')
 	{	
-		image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_0);
+		bitmap_layer_set_bitmap(image_layer, s_image_digit_0);
 	}
 	else if (character == '1')
 	{
-		image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_1);
+		bitmap_layer_set_bitmap(image_layer, s_image_digit_1);
 	}
 	else if (character == '2')
 	{
-		image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_2);
+		bitmap_layer_set_bitmap(image_layer, s_image_digit_2);
 	}
 	else if (character == '3')
 	{
-		image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_3);
+		bitmap_layer_set_bitmap(image_layer, s_image_digit_3);
 	}
 	else if (character == '4')
 	{
-		image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_4);
+		bitmap_layer_set_bitmap(image_layer, s_image_digit_4);
 	}
 	else if (character == '5')
 	{
-		image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_5);
+		bitmap_layer_set_bitmap(image_layer, s_image_digit_5);
 	}
 	else if (character == '6')
 	{
-		image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_6);
+		bitmap_layer_set_bitmap(image_layer, s_image_digit_6);
 	}
 	else if (character == '7')
 	{
-		image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_7);
+		bitmap_layer_set_bitmap(image_layer, s_image_digit_7);
 	}
 	else if (character == '8')
 	{
-		image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_8);
+		bitmap_layer_set_bitmap(image_layer, s_image_digit_8);
 	}
 	else if (character == '9')
 	{
-		image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_9);
+		bitmap_layer_set_bitmap(image_layer, s_image_digit_9);
 	}
-
-	bitmap_layer_set_bitmap(image_layer, image);
-}
-
-void clear_digits()
-{
-	gbitmap_destroy(s_image_first);
-	gbitmap_destroy(s_image_second);
-	gbitmap_destroy(s_image_third);
-	gbitmap_destroy(s_image_fourth);
 }
 
 void tick_handler(struct tm *tick_time, TimeUnits units_changed)
@@ -80,12 +77,10 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 		strftime(buffer, sizeof(buffer), "%I%M", tick_time);
 	}
 
-	clear_digits();
-
-	set_digit(s_image_layer_first, s_image_first, buffer[0]);
-	set_digit(s_image_layer_second, s_image_second, buffer[1]);
-	set_digit(s_image_layer_third, s_image_third, buffer[2]);
-	set_digit(s_image_layer_fourth, s_image_fourth, buffer[3]);
+	set_digit(s_image_layer_first, buffer[0]);
+	set_digit(s_image_layer_second, buffer[1]);
+	set_digit(s_image_layer_third, buffer[2]);
+	set_digit(s_image_layer_fourth, buffer[3]);
 }
 
 void populate_clock() // Initially populate the clock so the face doesn't start blank
@@ -102,6 +97,17 @@ static void window_load(Window *window)
 {
 	window_set_background_color(window, GColorBlack);
 
+	s_image_digit_0 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_0);
+	s_image_digit_1 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_1);
+	s_image_digit_2 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_2);
+	s_image_digit_3 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_3);
+	s_image_digit_4 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_4);
+	s_image_digit_5 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_5);
+	s_image_digit_6 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_6);
+	s_image_digit_7 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_7);
+	s_image_digit_8 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_8);
+	s_image_digit_9 = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_9);
+
 	s_image_layer_first = bitmap_layer_create(GRect(36, 6, 72, 36));
 	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_image_layer_first));
 	s_image_layer_second = bitmap_layer_create(GRect(36, 46, 72, 36));
@@ -117,13 +123,20 @@ static void window_load(Window *window)
 static void window_unload(Window *window)
 {
 	bitmap_layer_destroy(s_image_layer_first);
-	gbitmap_destroy(s_image_first);
 	bitmap_layer_destroy(s_image_layer_second);
-	gbitmap_destroy(s_image_second);
 	bitmap_layer_destroy(s_image_layer_third);
-	gbitmap_destroy(s_image_third);
 	bitmap_layer_destroy(s_image_layer_fourth);
-	gbitmap_destroy(s_image_fourth);
+
+	gbitmap_destroy(s_image_digit_0);
+	gbitmap_destroy(s_image_digit_1);
+	gbitmap_destroy(s_image_digit_2);
+	gbitmap_destroy(s_image_digit_3);
+	gbitmap_destroy(s_image_digit_4);
+	gbitmap_destroy(s_image_digit_5);
+	gbitmap_destroy(s_image_digit_6);
+	gbitmap_destroy(s_image_digit_7);
+	gbitmap_destroy(s_image_digit_8);
+	gbitmap_destroy(s_image_digit_9);
 }
 
 static void init(void)
