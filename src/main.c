4d7d6d5f8e9f15b38035c8b618a75ad934676984
Jesse Millar <hellojessemillar@gmail.com>
2014-12-30 12:42:05 -0600

    Doing everything with images

--- src/main.c
+++ src/main.c
@@ -2,18 +2,23 @@
 
 static Window *window;
 
-static BitmapLayer *h_bitmap_layer_background;
+static RotBitmapLayer *h_bitmap_layer_background;
 static GBitmap *h_bitmap_background;
-
-static GFont *h_font_helvetica;
-static TextLayer *h_text_layer_time;
 
 void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 {
-	static char buffer[9]; // Enough for XX:XXxXX with a trailing space
-	strftime(buffer, sizeof(buffer), "%H:%M", tick_time);
-	clock_copy_time_string(buffer, sizeof(buffer));
-	text_layer_set_text(g_text_layer_time, buffer);
+	static char buffer[3]; // Enough for XX with a trailing space
+
+	if (clock_is_24h_style() == true)
+	{
+		// Use 24 hour format
+		strftime(buffer, sizeof(buffer), "%k", tick_time);
+	}
+	else
+	{
+		// Use 12 hour format
+		strftime(buffer, sizeof(buffer), "%l", tick_time);
+	}
 }
 
 void populate_clock() // Initially populate the clock so the face doesn't start blank
@@ -28,26 +33,19 @@ void populate_clock() // Initially populate the clock so the face doesn't start 
 
 static void window_load(Window *window)
 {
-	h_bitmap_layer_background = bitmap_layer_create(GRect(-39, -27, 222, 222));
+	// h_bitmap_layer_background = rot_bitmap_layer_create(GRect(-39, -27, 222, 222));
 	h_bitmap_background = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
-	bitmap_layer_set_bitmap(h_bitmap_layer_background, h_bitmap_background);
-	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(h_bitmap_layer_background));
-
-	h_font_helvetica = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_HELVETICA_50));
-	h_text_layer_time = text_layer_create(GRect(0, 20, 144, 60));
-	text_layer_set_text(h_text_layer_time, "6");
-	text_layer_set_text_alignment(h_text_layer_time, GTextAlignmentCenter);
-	layer_add_child(window, text_layer_get_layer(h_text_layer_time));
+	h_bitmap_layer_background = rot_bitmap_layer_create(h_bitmap_background);
+	// bitmap_layer_set_bitmap(h_bitmap_layer_background, h_bitmap_background);
+	// layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(h_bitmap_layer_background));
 
 	populate_clock();
 }
 
 static void window_unload(Window *window)
 {
-	bitmap_layer_destroy(h_bitmap_layer_background);
+	rot_bitmap_layer_destroy(h_bitmap_layer_background);
 	gbitmap_destroy(h_bitmap_background);
-	text_layer_destroy(h_text_layer_time);
-	fonts_unload_custom_font(h_font_helvetica);
 }
 
 static void init(void)
